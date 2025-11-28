ca515e21d7456ac86114b7b6a69f72fb31d40b4d
egorvegera <egornixon@gmail.com>
2025-11-29 00:16:46 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/vue-project/',
+  assetsInclude: ['**/*.json'],
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
